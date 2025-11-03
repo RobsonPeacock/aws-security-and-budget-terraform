1779510c8599e3ec6cd9443365cb93dda5e8a508
Robson Peacock <robson_peacock20@outlook.com>
2025-11-03 22:48:42 +0000

    Add public access block for tf state bucket

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -25,6 +25,14 @@ resource "aws_s3_bucket_versioning" "tf_state_bucket_versioning" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "tf_state_bucket_block" {
+  bucket = aws_s3_bucket.tf_state_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_dynamodb_table" "tf_state_locking_table" {
   name         = "tf-state-locking"
   billing_mode = "PAY_PER_REQUEST"
