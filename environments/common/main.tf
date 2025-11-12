f6d6289594972d2232657b6c5ee74618e77106db
Robson Peacock <robson_peacock20@outlook.com>
2025-11-12 22:10:24 +0000

    Manage modules from common dir

--- environments/common/main.tf
+++ environments/common/main.tf
@@ -3,3 +3,9 @@ module "foundational_security" {
   account_id = var.account_id
   aws_region = var.aws_region
 }
+
+module "foundational_budget" {
+  source = "../../modules/budget"
+  account_id = var.account_id
+  notification_emails = var.notification_emails
+}
