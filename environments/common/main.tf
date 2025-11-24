faa19a39c845538119df268f87523c23fe8123f2
Robson Peacock <robson_peacock20@outlook.com>
2025-11-24 14:21:49 +0000

    Use aws_caller_identity data source for account ID

--- environments/common/main.tf
+++ environments/common/main.tf
@@ -1,11 +1,11 @@
 module "foundational_security" {
   source     = "../../modules/security"
-  account_id = var.account_id
+  account_id = local.account_id
   aws_region = var.aws_region
 }
 
 module "foundational_budget" {
   source              = "../../modules/budget"
-  account_id          = var.account_id
+  account_id          = local.account_id
   notification_emails = var.notification_emails
 }
--- environments/common/variables.tf
+++ environments/common/variables.tf
@@ -4,11 +4,6 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
-variable "account_id" {
-  description = "The ID of the AWS account"
-  type        = string
-}
-
 variable "notification_emails" {
   description = "A list of email addresses to receive budget alerts."
   type        = list(string)
